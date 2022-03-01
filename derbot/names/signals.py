c45ccc9b6d53c41859f89786647cc43be22768d6
Brian Dunnette <dunn0172@umn.edu>
2022-03-01 09:40:15 -0600

    fixing toot delay, adding task to generate missing jerseys

--- derbot/names/signals.py
+++ derbot/names/signals.py
@@ -13,9 +13,6 @@ def set_missing_number(sender, instance, *args, **kwargs):
     # Ensure that DerbyNumber is set for non-registered names
     if not instance.registered and not instance.number:
         instance.number = DerbyNumber.objects.filter(cleared=True).order_by("?").first()
-    # Regenerate tank if number changed
-    if instance.number_tracker.has_changed("number") and instance.number:
-        generate_tank.delay(instance.id, overwrite=True)
 
 
 @receiver(post_save, sender=DerbyName)
@@ -23,3 +20,6 @@ def generate_missing_tank(sender, instance, *args, **kwargs):
     if instance.cleared and instance.jersey is None:
         logger.info(f"{sender}: Generating missing tank for {instance.name}")
         generate_tank.delay(instance.id)
+    # Regenerate tank if number changed
+    if instance.number_tracker.has_changed("number") and instance.number:
+        generate_tank.delay(instance.id, overwrite=True)
